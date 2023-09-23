92a1bf4ce6652a7e1b66da0eb842eb7ccd1e62b1
Lucas Miranda <mirandalucas52@gmail.com>
2023-09-23 20:08:11 +0200

    accueil fini

--- src/components/ReactSlickTestimonials/index.jsx
+++ src/components/ReactSlickTestimonials/index.jsx
@@ -21,48 +21,48 @@ export const SliderTestimonials = () => {
             <Slider {...settings}>
                 <div>
                     <p>
-                        Nous avons fait appel à l'entreprise pour la réfection
-                        de la peinture de notre appartement et nous sommes
-                        tombés sur une super équipe, très pro et de bons
-                        conseils. Un très bon relationnel tout au long des
-                        travaux. La peinture conseillée est de très bonne
-                        qualité et le rendu final est superbe. Merci encore !
+                        " Intervention pour le remplacement d'une porte de
+                        copropriété sur mesure. Mr Athane a su nous conseiller
+                        et nous orienter vers une porte ressemblante à
+                        l'ancienne pour conserver l'esthétique original. Le
+                        montage a été parfaitement réalisé. Je recommande
+                        vivement. "
                     </p>
-                    <span>C M</span>
+                    <span>Sylvain Leroy</span>
                 </div>
                 <div>
                     <p>
-                        Nous avons fait appel à Habitat Services pour le
+                        " Nous avons fait appel à Habitat Services pour le
                         remplacement de deux portes donnant sur notre véranda.
                         Le résultat est à la hauteur de nos espérances. Rendu
                         très propre et matériau de qualité. Notre pièce à vivre
                         a même gagné en clarté. Un très bon contact préalable
                         pour bien comprendre nos besoins, et une réalisation
                         rapide et sur mesure. Bref, nous sommes ravis et
-                        recommandons les services de M. Athane et son équipe !
+                        recommandons les services de M. Athane et son équipe ! "
                     </p>
                     <span>Babi King</span>
                 </div>
                 <div>
                     <p>
-                        Intervention pour le remplacement d'une porte de
-                        copropriété sur mesure. Mr Athane a su nous conseiller
-                        et nous orienter vers une porte ressemblante à
-                        l'ancienne pour conserver l'esthétique original. Le
-                        montage a été parfaitement réalisé. Je recommande
-                        vivement.
+                        " Nous avons fait appel à l'entreprise pour la réfection
+                        de la peinture de notre appartement et nous sommes
+                        tombés sur une super équipe, très pro et de bons
+                        conseils. Un très bon relationnel tout au long des
+                        travaux. La peinture conseillée est de très bonne
+                        qualité et le rendu final est superbe. Merci encore ! "
                     </p>
-                    <span>Sylvain Leroy</span>
+                    <span>C M</span>
                 </div>
                 <div>
                     <p>
-                        Entreprise ayant fait la peinture de toutes les surfaces
-                        de ma maison, pose des meubles de sdb, création de
-                        placards sur mesure dans les chambres, pose d'un volet
-                        roulant extérieur et rénovation du parquet à l'étage
-                        (ponçage + nouvelle teinte). Travail propre dans
+                        " Entreprise ayant fait la peinture de toutes les
+                        surfaces de ma maison, pose des meubles de sdb, création
+                        de placards sur mesure dans les chambres, pose d'un
+                        volet roulant extérieur et rénovation du parquet à
+                        l'étage (ponçage + nouvelle teinte). Travail propre dans
                         l'ensemble. Se renseigner sur les délais qui peuvent
-                        être légèrement plus longs que prévu.
+                        être légèrement plus longs que prévu. "
                     </p>
                     <span>Sandor Blasek</span>
                 </div>
--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -1,9 +1,44 @@
 import "./Footer.css";
+import logo from "../../assets/logo_habitat_services_menuiseries_dark.svg";
 
 function Footer() {
     return (
         <footer>
-            <div>footer</div>
+            <div className="footerContainer">
+                <img src={logo} alt="" />
+                <ul className="footerAddress">
+                    <li>Habitat Services Menuiseries</li>
+                    <li>1 avenue du Marechal Foch,</li>
+                    <li>31110 Bagnères de Luchon,</li>
+                    <li>France</li>
+                    <a href="tel:06 51 85 57 93">06 51 85 57 93</a>
+                </ul>
+                <ul className="footerLinks">
+                    <span>
+                        <li>
+                            <a href="/">Accueil</a>
+                        </li>
+                        <li>
+                            <a href="/about">À propos</a>
+                        </li>
+                        <li>
+                            <a href="/services">Prestations</a>
+                        </li>
+                    </span>
+                    <span>
+                        <li>
+                            <a href="/projects">Chantiers</a>
+                        </li>
+                        <li>
+                            <a href="/news">Actualités</a>
+                        </li>
+                        <li>
+                            <a href="/contact">Contact</a>
+                        </li>
+                    </span>
+                </ul>
+            </div>
+            <p>© Habitat Services Menuiseries. Tous droit résérvés</p>
         </footer>
     );
 }
