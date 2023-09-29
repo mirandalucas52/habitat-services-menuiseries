549d36ccc9975801bc562d50050362c1259973f9
Lucas Miranda <mirandalucas52@gmail.com>
2023-09-29 15:10:59 +0200

    contact form fixed

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,129 +1,56 @@
-import React, { Component } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./ContactForm.css";
 
-class ContactForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstName: "",
-            lastName: "",
-            email: "",
-            phone: "",
-            message: "",
-            objet: "",
-        };
-    }
+export const ContactForm = () => {
+    const form = useRef();
 
-    handleInputChange = (e) => {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
-    };
-
-    handleSubmit = (e) => {
+    const sendEmail = (e) => {
         e.preventDefault();
 
-        const { firstName, lastName, email, phone, message, objet } =
-            this.state;
-
-        fetch("/send-email", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                firstName,
-                lastName,
-                email,
-                phone,
-                message,
-                objet,
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                // Vous pouvez ajouter ici une logique pour afficher un message de succès à l'utilisateur
-            })
-            .catch((error) => {
-                console.error(error);
-                // Vous pouvez ajouter ici une logique pour afficher un message d'erreur à l'utilisateur
-            });
+        emailjs
+            .sendForm(
+                "service_7x41xoh",
+                "template_7w0w8n6",
+                form.current,
+                "YO2Sy70bfI-O562iR"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
     };
 
-    render() {
-        return (
-            <div>
-                <h2>Contactez-nous</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label htmlFor="firstName">Prénom:</label>
-                        <input
-                            type="text"
-                            id="firstName"
-                            name="firstName"
-                            value={this.state.firstName}
-                            onChange={this.handleInputChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="lastName">Nom:</label>
-                        <input
-                            type="text"
-                            id="lastName"
-                            name="lastName"
-                            value={this.state.lastName}
-                            onChange={this.handleInputChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="objet">Objet:</label>
-                        <input
-                            type="text"
-                            id="objet"
-                            name="objet"
-                            value={this.state.objet}
-                            onChange={this.handleInputChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="email">E-mail:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.handleInputChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="phone">Téléphone:</label>
-                        <input
-                            type="tel"
-                            id="phone"
-                            name="phone"
-                            value={this.state.phone}
-                            onChange={this.handleInputChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="message">Message:</label>
-                        <textarea
-                            id="message"
-                            name="message"
-                            value={this.state.message}
-                            onChange={this.handleInputChange}
-                            required
-                        ></textarea>
-                    </div>
-                    <button type="submit">Envoyer</button>
-                </form>
+    return (
+        <form className="contactForm" ref={form} onSubmit={sendEmail}>
+            <div className="contactDisplay">
+                <span>
+                    <input
+                        placeholder="VOTRE NOM:"
+                        type="text"
+                        name="from_name"
+                    />
+
+                    <input
+                        placeholder="VOTRE EMAIL:"
+                        type="email"
+                        name="from_email"
+                    />
+                </span>
+                <span>
+                    <input placeholder="VOTRE TELEPHONE:" name="from_phone" />
+
+                    <input placeholder="OBJET:" name="from_obj" />
+                </span>
             </div>
-        );
-    }
-}
+            <textarea placeholder="VOTRE MESSAGE:" name="from_msg" />
+            <input type="submit" value="Send" />
+        </form>
+    );
+};
 
 export default ContactForm;
